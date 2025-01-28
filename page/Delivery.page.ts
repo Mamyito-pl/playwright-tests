66a4df7c5206eb12216fb734b672f83a1a6a9b29
Daniel Lalak <daniel.lalak@outlook.com>
2025-01-28 20:59:46 +0100

    Added tests for delivery

--- page/Delivery.page.ts
+++ page/Delivery.page.ts
@@ -12,11 +12,162 @@ export default class DeliveryPage {
         this.mobile = isMobile(viewport.width);
     }
 
-    async clickDeliverySlotButton() {
-        await this.page.click(selectors.DeliveryPage.common.deliverySlot);
+    async clickAddNewAddressButton() {
+        await this.getAddNewAddressButton.click();
+    }
+
+    async clickAddNewInvoiceAddressButton() {
+        await this.getAddNewInvoiceAddressButton.click();
     }
 
     get getDeliverySlotButton() {
-        return this.page.locator(selectors.DeliveryPage.common.deliverySlot).first();
+        return this.page.locator(selectors.DeliveryPage.common.deliverySlot);
+    }
+
+    get getDeliveryAddressTitle() {
+        return this.page.locator('div[class*="ghRAau"]');
+    }
+
+    get getDeliveryAddressSubTitle() {
+        return this.page.getByText('Wybierz adres dostawy lub dodaj nowy');
+    }
+
+    get getAddNewAddressButton() {
+        return this.page.getByText('Dodaj nowy adres');
+    }
+
+    get getAddNewInvoiceAddressButton() {
+        return this.page.locator(`button:has-text('Dodaj nowy podmiot')`)
+    }
+
+    get getDeliveryInvoiceCheckbox() {
+        return this.page.getByText('Chcę otrzymać F-Vat');
+    }
+
+    get getDeliveryDateTitle() {
+        return this.page.getByText('Termin dostawy');
+    }
+
+    // Address/Invoice Modal
+
+    async clickSaveAdressModalButton() {
+        await this.getAddressModalSaveButton.click();
+    }
+
+    get getAddressModalCloseIcon() {
+        return this.page.locator(selectors.DeliveryPage.common.AddressModalCloseIcon);
+    }
+
+    get getAddressModalSaveButton() {
+        return this.page.getByText('Zapisz');
+    }
+
+    get getAddressModalCancelButton() {
+        return this.page.getByText('Anuluj');
+    }
+
+    get getAddressModalConfirmationButton() {
+        return this.page.locator(`button:has-text("Potwierdź")`);
+    }
+
+    getAddressModalDeleteAddressName(addressName) {
+        return this.page.locator(`div[data-sentry-element="AddressName"]:has-text("${addressName}")`);
+    }
+
+
+    // Address Modal
+
+    get getAddressModal() {
+        return this.page.locator('div[class*="gHrfft"]');
+    }
+
+    get getAddressModalAddressName() {
+        return this.page.locator('#delivery_address_name');
+    }
+
+    get getAddressModalAddressSearchAddress() {
+        return this.page.locator('#:r16:');
+    }
+
+    get getAddressModalUserName() {
+        return this.page.locator('#delivery_address_first_name');
+    }
+
+    get getAddressModalUserSurname() {
+        return this.page.locator('#delivery_address_last_name');
+    }
+
+    get getAddressModalUserPhoneNumber() {
+        return this.page.locator('#delivery_address_phone');
+    }
+
+    get getAddressModalUserPostalCode() {
+        return this.page.locator('#delivery_address_postal');
+    }
+
+    get getAddressModalUserCity() {
+        return this.page.locator('#delivery_address_city');
+    }
+
+    get getAddressModalUserStreet() {
+        return this.page.locator('#delivery_address_street');
+    }
+
+    get getAddressModalUserHouseNumber() {
+        return this.page.locator('#delivery_address_house_number');
+    }
+
+    get getAddressModalUserStaircase() {
+        return this.page.locator('#delivery_address_staircase');
+    }
+
+    get getAddressModalUserFlatNumber() {
+        return this.page.locator('#delivery_address_flat_number');
+    }
+
+    get getAddressModalUserFloor() {
+        return this.page.locator('#delivery_address_floor');
+    }
+
+    get getAddressModalUserDeliveryNotes() {
+        return this.page.locator('#delivery_address_floor');
+    }
+
+    // Invoice Address Modal
+
+    get getInvoiceAddressModalAddressName() {
+        return this.page.locator('#invoice_address_name');
+    }
+
+    get getInvoiceAddressModalAddressSearchAddress() {
+        return this.page.locator('#:rku:');
+    }
+
+    get getInvoiceAddressModalCompanyName() {
+        return this.page.locator('#invoice_address_company_name');
+    }
+
+    get getInvoiceAddressModalNIP() {
+        return this.page.locator('#invoice_address_tax');
+    }
+
+    get getInvoiceAddressModalUserPostalCode() {
+        return this.page.locator('#invoice_address_postal');
+    }
+
+    get getInvoiceAddressModalUserCity() {
+        return this.page.locator('#invoice_address_city');
+    }
+
+    get getInvoiceAddressModalUserStreet() {
+        return this.page.locator('#invoice_address_street');
+    }
+
+    get getInvoiceAddressModalUserHouseNumber() {
+        return this.page.locator('#invoice_address_house_number');
+    }
+
+    get getInvoiceAddressModalUserFlatNumber() {
+        return this.page.locator('#invoice_address_flat_number');
     }
 }
---
+++ tests/mobile/Delivery.spec.ts
@@ -0,0 +1,380 @@
+import { expect } from '@playwright/test';
+import CommonPage from "../../page/Common.page";
+import DeliveryPage from '../../page/Delivery.page.ts';
+import * as allure from "allure-js-commons";
+import * as selectors from '../../utils/selectors.json';
+import { test } from '../../fixtures/fixtures.ts';
+
+test.describe.configure({ mode: 'serial'})
+
+test.describe('Testy dostawy', async () => {
+
+  let commonPage: CommonPage;
+  let deliveryPage : DeliveryPage;
+
+  test.beforeEach(async ({ page, loginManual }) => {
+
+    await allure.tags("Mobilne", "Dostawa")
+    await allure.parentSuite("Mobilne");
+    await allure.suite("Dostawa");
+
+    await loginManual();
+
+    commonPage = new CommonPage(page);
+    deliveryPage = new DeliveryPage(page);
+  })
+  
+  test('M | Okno dostawy otwiera się ze wszystkimi potrzebnymi polami', async ({ page }) => {
+
+    await page.goto('/dostawa', { waitUntil: 'domcontentloaded' });
+
+    await expect(deliveryPage.getDeliveryAddressTitle).toBeVisible();
+    await expect(deliveryPage.getDeliveryAddressSubTitle).toBeVisible();
+    await expect(deliveryPage.getAddNewAddressButton).toBeVisible();
+    await expect(deliveryPage.getDeliveryInvoiceCheckbox).toBeVisible();
+    await expect(deliveryPage.getDeliveryDateTitle).toBeVisible();
+  })
+
+  test('M | Możliwość wyboru terminu dostawy', async ({ page }) => {
+
+    await page.goto('/dostawa', { waitUntil: 'domcontentloaded' });
+    await page.waitForSelector(selectors.DeliveryPage.common.deliverySlot, { timeout: 15000, state: 'visible' });
+
+    await expect(deliveryPage.getDeliverySlotButton.first()).toContainText('Dostępny');
+    await deliveryPage.getDeliverySlotButton.first().click();
+    await expect(deliveryPage.getDeliverySlotButton.first()).toContainText('Wybrany');
+    await deliveryPage.getDeliverySlotButton.last().click();
+    await expect(deliveryPage.getDeliverySlotButton.first()).toContainText('Dostępny');
+    await expect(deliveryPage.getDeliverySlotButton.last()).toContainText('Wybrany');
+  })
+
+  test.describe('Adres dostawy', async () => {
+    
+    test('M | Możliwość dodania adresu dostawy', async ({ page }) => {
+
+      test.setTimeout(100000);
+      
+      await page.goto('/dostawa', { waitUntil: 'networkidle' });
+
+      await deliveryPage.clickAddNewAddressButton();
+      await expect(deliveryPage.getAddressModal).toBeVisible();
+      await expect(deliveryPage.getAddressModal).toContainText('Dodaj nowy adres');
+
+      await expect(deliveryPage.getAddressModalAddressName).toBeVisible();
+      await deliveryPage.getAddressModalAddressName.fill('Adres Testowy');
+
+      await expect(deliveryPage.getAddressModalUserName).toBeVisible();
+      await deliveryPage.getAddressModalUserName.fill('Jan');
+
+      await expect(deliveryPage.getAddressModalUserSurname).toBeVisible();
+      await deliveryPage.getAddressModalUserSurname.fill('Kowalski')
+
+      await expect(deliveryPage.getAddressModalUserPhoneNumber).toBeVisible();
+      await deliveryPage.getAddressModalUserPhoneNumber.fill('555666777');
+
+      await expect(deliveryPage.getAddressModalUserPostalCode).toBeVisible();
+      await deliveryPage.getAddressModalUserPostalCode.fill('00-133');
+
+      await expect(deliveryPage.getAddressModalUserCity).toBeVisible();
+      await deliveryPage.getAddressModalUserCity.fill('Warszawa');
+
+      await expect(deliveryPage.getAddressModalUserStreet).toBeVisible();
+      await deliveryPage.getAddressModalUserStreet.fill('aleja Jana Pawła II');
+
+      await expect(deliveryPage.getAddressModalUserHouseNumber).toBeVisible();
+      await deliveryPage.getAddressModalUserHouseNumber.fill('20');
+
+      await expect(deliveryPage.getAddressModalUserStaircase).toBeVisible();
+      await deliveryPage.getAddressModalUserStaircase.fill('1');
+
+      await expect(deliveryPage.getAddressModalUserFlatNumber).toBeVisible();
+      await deliveryPage.getAddressModalUserFlatNumber.fill('30');
+
+      /*await expect(deliveryPage.getAddressModalUserFloor).toBeVisible();
+      await deliveryPage.getAddressModalUserFloor.fill('2');
+                                                                                        // Uncomment after done task KAN-801
+      await expect(deliveryPage.getAddressModalUserDeliveryNotes).toBeVisible();
+      await deliveryPage.getAddressModalUserDeliveryNotes.fill('Testowa notatka');*/
+
+      await expect(deliveryPage.getAddressModalSaveButton).toBeVisible();
+      await deliveryPage.clickSaveAdressModalButton();
+
+      await expect(commonPage.getMessage).toHaveText("Dane zostały zapisane", { timeout: 5000 })
+
+      await page.waitForSelector('text=Adres Testowy', { state: 'visible' });
+    })
+
+    test('M | Możliwość wyboru adresu dostawy', async ({ page, addAddressDelivery, deleteAddressDelivery }) => {
+
+      test.setTimeout(100000);
+
+      const targetAddress = page.getByText('Adres Testowy').locator('..').locator('..').locator('..');
+      
+      await page.goto('/dostawa', { waitUntil: 'domcontentloaded' });
+
+      await addAddressDelivery('Adres Fixturowy');
+
+      await page.getByText('Adres Testowy').click();
+
+      await expect(targetAddress).toContainText('Aktualnie wybrany', { timeout: 5000 });
+
+      const borderColor = await targetAddress.evaluate((el) => {
+        const styles = window.getComputedStyle(el);
+        return styles.getPropertyValue('border'); 
+      });
+
+      console.log('Kolor obramowania:', borderColor);
+      expect(borderColor).toBe('1px solid rgb(78, 180, 40)');
+
+      await page.getByText('Adres Fixturowy').click({ force: true });
+
+      await expect(targetAddress).not.toContainText('Aktualnie wybrany', { timeout: 5000 });
+
+      await deleteAddressDelivery('Adres Testowy');
+    })
+
+    test('M | Możliwość edycji adresu dostawy', async ({ page }) => {
+
+      test.setTimeout(100000);
+
+      await page.goto('/dostawa', { waitUntil: 'domcontentloaded' });
+
+      await page.getByText('Adres Fixturowy').locator('..').locator('..').locator('..').locator('svg').nth(1).click();
+
+      await expect(deliveryPage.getAddressModal).toBeVisible();
+      await expect(deliveryPage.getAddressModal).toContainText('Edytuj adres');
+
+      await expect(deliveryPage.getAddressModalAddressName).toHaveValue('Adres Fixturowy')
+      await deliveryPage.getAddressModalAddressName.fill('Adres Edytowany');
+
+      await expect(deliveryPage.getAddressModalUserName).toHaveValue('Jan');
+      await deliveryPage.getAddressModalUserName.fill('Jan1');
+
+      await expect(deliveryPage.getAddressModalUserSurname).toHaveValue('Kowalski');
+      await deliveryPage.getAddressModalUserSurname.fill('Kowalski1');
+
+      await expect(deliveryPage.getAddressModalUserPhoneNumber).toHaveValue('555666777');
+      await deliveryPage.getAddressModalUserPhoneNumber.fill('777666555');
+
+      await expect(deliveryPage.getAddressModalUserPostalCode).toHaveValue('00-828');
+      await deliveryPage.getAddressModalUserPostalCode.fill('05-506');
+
+      await expect(deliveryPage.getAddressModalUserCity).toHaveValue('Warszawa');
+      await deliveryPage.getAddressModalUserCity.fill('Lesznowola');
+
+      await expect(deliveryPage.getAddressModalUserStreet).toHaveValue('aleja Jana Pawła II');
+      await deliveryPage.getAddressModalUserStreet.fill('Oficerska');
+
+      await expect(deliveryPage.getAddressModalUserHouseNumber).toHaveValue('1');
+      await deliveryPage.getAddressModalUserHouseNumber.fill('4');
+
+      await expect(deliveryPage.getAddressModalUserStaircase).toHaveValue('1');
+      await deliveryPage.getAddressModalUserStaircase.fill('2');
+
+      await expect(deliveryPage.getAddressModalUserFlatNumber).toHaveValue('30');
+      await deliveryPage.getAddressModalUserFlatNumber.fill('3');
+
+      /*await expect(deliveryPage.getAddressModalUserFloor).toHaveValue('x');
+      await deliveryPage.getAddressModalUserFloor.fill('4');
+                                                                                        // Uncomment after done task KAN-801
+      await expect(deliveryPage.getAddressModalUserDeliveryNotes).toHaveValue('x');
+      await deliveryPage.getAddressModalUserDeliveryNotes.fill('Edytowana testowa notatka');*/
+
+      await expect(deliveryPage.getAddressModalSaveButton).toBeVisible();
+      await deliveryPage.clickSaveAdressModalButton();
+
+      await expect(commonPage.getMessage).toHaveText('Adres "Adres Edytowany" został zaktualizowany.', { timeout: 5000 });
+
+      await page.getByText('Adres Edytowany').locator('..').locator('..').locator('..').locator('svg').nth(1).click();
+
+      await expect(deliveryPage.getAddressModal).toBeVisible();
+      await expect(deliveryPage.getAddressModal).toContainText('Edytuj adres');
+      await expect(deliveryPage.getAddressModalAddressName).toHaveValue('Adres Edytowany')
+      await expect(deliveryPage.getAddressModalUserName).toHaveValue('Jan1');
+      await expect(deliveryPage.getAddressModalUserSurname).toHaveValue('Kowalski1');
+      await expect(deliveryPage.getAddressModalUserPhoneNumber).toHaveValue('777666555');
+      await expect(deliveryPage.getAddressModalUserPostalCode).toHaveValue('05-506');
+      await expect(deliveryPage.getAddressModalUserCity).toHaveValue('Lesznowola');
+      await expect(deliveryPage.getAddressModalUserStreet).toHaveValue('Oficerska');
+      await expect(deliveryPage.getAddressModalUserHouseNumber).toHaveValue('4');
+      await expect(deliveryPage.getAddressModalUserStaircase).toHaveValue('2');
+      await expect(deliveryPage.getAddressModalUserFlatNumber).toHaveValue('3');
+      /*await expect(deliveryPage.getAddressModalUserFloor).toHaveValue('77');
+                                                                                        // Uncomment after done task KAN-801
+      await expect(deliveryPage.getAddressModalUserDeliveryNotes).toHaveValue('88');
+      */
+    })
+    
+    test('M | Możliwość usunięcia adresu dostawy', async ({ page }) => {
+
+      await page.goto('/dostawa', { waitUntil: 'networkidle' });
+
+      await page.getByText('Adres Edytowany').locator('..').locator('..').locator('..').locator('svg').nth(2).click();
+
+      await expect(deliveryPage.getAddressModal).toBeVisible();
+      await expect(deliveryPage.getAddressModal).toContainText('Potwierdź usunięcie adresu');
+      await expect(deliveryPage.getAddressModalDeleteAddressName('Adres Edytowany')).toContainText('Adres Edytowany');
+      await expect(deliveryPage.getAddressModalCancelButton).toBeVisible();
+      await expect(deliveryPage.getAddressModalConfirmationButton).toBeVisible();
+      await deliveryPage.getAddressModalConfirmationButton.click();
+      await expect(commonPage.getMessage).toHaveText('Adres "Adres Edytowany" został usunięty.', { timeout: 5000 })
+
+      await page.waitForSelector('text=Adres Edytowany', { state: 'hidden' });
+    })
+  })
+
+  test.describe('Faktura', async () => {
+    
+    test('M | Możliwość dodania podmiotu do faktury', async ({ page }) => {
+      
+      test.setTimeout(120000);
+      
+      await page.goto('/dostawa', { waitUntil: 'networkidle' });
+
+      await deliveryPage.getDeliveryInvoiceCheckbox.isVisible();
+      await deliveryPage.getDeliveryInvoiceCheckbox.check();
+      await deliveryPage.getDeliveryInvoiceCheckbox.isChecked();
+      await deliveryPage.clickAddNewInvoiceAddressButton();
+      await expect(deliveryPage.getAddressModal).toBeVisible();
+      await expect(deliveryPage.getAddressModal).toContainText('Dodaj nowy podmiot');
+
+      await expect(deliveryPage.getInvoiceAddressModalAddressName).toBeVisible();
+      await deliveryPage.getInvoiceAddressModalAddressName.fill('Testowa nazwa podmiotu');
+
+      await expect(deliveryPage.getInvoiceAddressModalCompanyName).toBeVisible();
+      await deliveryPage.getInvoiceAddressModalCompanyName.fill('Testowa nazwa firmy');
+
+      await expect(deliveryPage.getInvoiceAddressModalNIP).toBeVisible();
+      await deliveryPage.getInvoiceAddressModalNIP.fill('8140667487')
+
+      await expect(deliveryPage.getInvoiceAddressModalUserPostalCode).toBeVisible();
+      await deliveryPage.getInvoiceAddressModalUserPostalCode.fill('00-133');
+
+      await expect(deliveryPage.getInvoiceAddressModalUserCity).toBeVisible();
+      await deliveryPage.getInvoiceAddressModalUserCity.fill('Warszawa');
+
+      await expect(deliveryPage.getInvoiceAddressModalUserStreet).toBeVisible();
+      await deliveryPage.getInvoiceAddressModalUserStreet.fill('aleja Jana Pawła II');
+
+      await expect(deliveryPage.getInvoiceAddressModalUserHouseNumber).toBeVisible();
+      await deliveryPage.getInvoiceAddressModalUserHouseNumber.fill('10');
+
+      await expect(deliveryPage.getInvoiceAddressModalUserFlatNumber).toBeVisible();
+      await deliveryPage.getInvoiceAddressModalUserFlatNumber.fill('12');
+
+      await expect(deliveryPage.getAddressModalSaveButton).toBeVisible();
+      await deliveryPage.clickSaveAdressModalButton();
+
+      await expect(commonPage.getMessage).toHaveText('Dane zostały zapisane', { timeout: 5000 })
+
+      await page.waitForSelector('text=Testowa nazwa podmiotu', { timeout: 10000, state: 'visible' });
+    })
+
+    test('M | Możliwość wyboru podmiotu do faktury', async ({ page, addInvoiceAddressDelivery, deleteInvoiceAddressDelivery }) => {
+      
+      test.setTimeout(100000);
+
+      const targetAddress = page.getByText('Testowa nazwa podmiotu').locator('..').locator('..').locator('..');
+      
+      await page.goto('/dostawa', { waitUntil: 'domcontentloaded' });
+
+      await addInvoiceAddressDelivery('Fixturowy adres podmiotu');
+
+      await page.getByText('Testowa nazwa podmiotu').click();
+
+      await expect(targetAddress).toContainText('Aktualnie wybrany', { timeout: 3000 });
+
+      const borderColor = await targetAddress.evaluate((el) => {
+        const styles = window.getComputedStyle(el);
+        return styles.getPropertyValue('border'); 
+      });
+
+      console.log('Kolor obramowania:', borderColor);
+      expect(borderColor).toBe('1px solid rgb(78, 180, 40)');
+
+      await page.getByText('Fixturowy adres podmiotu').click();
+
+      await expect(targetAddress).not.toContainText('Aktualnie wybrany', { timeout: 3000 });
+
+      await deleteInvoiceAddressDelivery('Testowa nazwa podmiotu');
+    })
+
+    test('M | Możliwość edycji podmiotu do faktury', async ({ page }) => {
+      
+      test.setTimeout(120000);
+
+      await page.goto('/dostawa', { waitUntil: 'networkidle' });
+
+      await deliveryPage.getDeliveryInvoiceCheckbox.isVisible();
+      await deliveryPage.getDeliveryInvoiceCheckbox.check();
+      await deliveryPage.getDeliveryInvoiceCheckbox.isChecked();
+
+      await page.waitForSelector('text=Fixturowy adres podmiotu', { timeout: 5000, state: 'visible' });
+
+      await page.getByText('Fixturowy adres podmiotu').locator('..').locator('..').locator('..').locator('svg').nth(1).click();
+
+      await expect(deliveryPage.getAddressModal).toBeVisible();
+      await expect(deliveryPage.getAddressModal).toContainText('Edytuj podmiot');
+
+      await expect(deliveryPage.getInvoiceAddressModalAddressName).toHaveValue('Fixturowy adres podmiotu');
+      await deliveryPage.getInvoiceAddressModalAddressName.fill('Edytowana nazwa podmiotu');
+
+      await expect(deliveryPage.getInvoiceAddressModalCompanyName).toHaveValue('Testowa firma');
+      await deliveryPage.getInvoiceAddressModalCompanyName.fill('Edytowana nazwa firmy');
+
+      await expect(deliveryPage.getInvoiceAddressModalNIP).toHaveValue('8140667487');
+      await deliveryPage.getInvoiceAddressModalNIP.fill('1085179548')
+
+      await expect(deliveryPage.getInvoiceAddressModalUserPostalCode).toHaveValue('00-828');
+      await deliveryPage.getInvoiceAddressModalUserPostalCode.fill('00-100');
+
+      await expect(deliveryPage.getInvoiceAddressModalUserCity).toHaveValue('Warszawa');
+      await deliveryPage.getInvoiceAddressModalUserCity.fill('Magdalenka');
+
+      await expect(deliveryPage.getInvoiceAddressModalUserStreet).toHaveValue('aleja Jana Pawła II');
+      await deliveryPage.getInvoiceAddressModalUserStreet.fill('Edytowana ulica');
+
+      await expect(deliveryPage.getInvoiceAddressModalUserHouseNumber).toHaveValue('1');
+      await deliveryPage.getInvoiceAddressModalUserHouseNumber.fill('100');
+
+      await expect(deliveryPage.getInvoiceAddressModalUserFlatNumber).toHaveValue('30');
+      await deliveryPage.getInvoiceAddressModalUserFlatNumber.fill('200');
+
+      await expect(deliveryPage.getAddressModalSaveButton).toBeVisible();
+      await deliveryPage.clickSaveAdressModalButton();
+
+      await expect(commonPage.getMessage).toHaveText('Adres "Edytowana nazwa podmiotu" został zaktualizowany.', { timeout: 5000 })
+      await expect(commonPage.getMessage).not.toBeVisible({ timeout: 10000 })
+      
+      await page.waitForSelector('text=Edytowana nazwa podmiotu', { timeout: 5000, state: 'visible' });
+
+      await page.getByText('Edytowana nazwa podmiotu').locator('..').locator('..').locator('..').locator('svg').nth(1).click({ force: true });
+
+      await expect(deliveryPage.getAddressModal).toBeVisible();
+      await expect(deliveryPage.getAddressModal).toContainText('Edytuj podmiot');
+      await expect(deliveryPage.getInvoiceAddressModalAddressName).toHaveValue('Edytowana nazwa podmiotu')
+      await expect(deliveryPage.getInvoiceAddressModalCompanyName).toHaveValue('Edytowana nazwa firmy');
+      await expect(deliveryPage.getInvoiceAddressModalNIP).toHaveValue('1085179548');
+      await expect(deliveryPage.getInvoiceAddressModalUserPostalCode).toHaveValue('00-100');
+      await expect(deliveryPage.getInvoiceAddressModalUserCity).toHaveValue('Magdalenka');
+      await expect(deliveryPage.getInvoiceAddressModalUserStreet).toHaveValue('Edytowana ulica');
+      await expect(deliveryPage.getInvoiceAddressModalUserHouseNumber).toHaveValue('100');
+      await expect(deliveryPage.getInvoiceAddressModalUserFlatNumber).toHaveValue('200');
+    })
+    
+    test('M | Możliwość usunięcia podmiotu do faktury', async ({ page }) => {
+      
+      await page.goto('/dostawa', { waitUntil: 'networkidle' });
+
+      await page.getByText('Edytowana nazwa podmiotu').locator('..').locator('..').locator('..').locator('svg').nth(2).click();
+
+      await expect(deliveryPage.getAddressModal).toBeVisible({ timeout: 3000 });
+      await expect(deliveryPage.getAddressModal).toContainText('Potwierdź usunięcie adresu');
+      await expect(deliveryPage.getAddressModalDeleteAddressName('Edytowana nazwa podmiotu')).toContainText('Edytowana nazwa podmiotu');
+      await expect(deliveryPage.getAddressModalCancelButton).toBeVisible();
+      await expect(deliveryPage.getAddressModalConfirmationButton).toBeVisible();
+      await deliveryPage.getAddressModalConfirmationButton.click();
+      await expect(commonPage.getMessage).toHaveText('Adres "Edytowana nazwa podmiotu" został usunięty.', { timeout: 5000 })
+    })
+  })
+})
