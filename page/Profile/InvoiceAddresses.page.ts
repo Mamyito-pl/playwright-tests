e33ab1829574c65ba75c9ed639bc62dafb6c07df
Daniel Lalak <daniel.lalak@outlook.com>
2025-03-25 15:41:35 +0100

    Added new tests for invoice addresses

---
+++ page/Profile/InvoiceAddresses.page.ts
@@ -0,0 +1,116 @@
+import { Page } from "@playwright/test";
+import * as selectors from '../../utils/selectors.json';
+import { isMobile } from '../../utils/utility-methods.ts';
+
+export default class InvoiceAddressesPage {
+    private mobile: boolean;
+
+    constructor(public page: Page) {
+        this.page = page;
+        const viewport = page.viewportSize();
+        if (!viewport) throw new Error('Viewport is null');
+        this.mobile = isMobile(viewport.width);
+    }
+
+    async clickAddNewInvoiceAddressButton() {
+        await this.getAddNewInvoiceAddressButton.click({ force: true });
+    }
+
+    async clickEditInvoiceAddressButton(addressName: string) {
+        return this.page.getByText(addressName).locator('..').locator('..').locator('svg[class="tabler-icon tabler-icon-pencil"]').click();
+    }
+
+    async clickDeleteInvoiceAddressButton(addressName: string) {
+        return this.page.getByText(addressName).locator('..').locator('..').locator('..').locator('svg[class="tabler-icon tabler-icon-trash"]').click();
+    }
+
+    getMainInvoiceAddressInfo(addressName: string) {
+        return this.page.getByText(addressName).locator('..').locator('..').locator('div[class*="sc-ad97832e-2"]');
+    }
+
+    get getCurrentMainInvoiceAddressModalInfo() {
+        return this.page.getByText('Obecnie wybrany adres główny');
+    }
+
+    // Invoice Address Modal
+
+    async clickSaveInvoiceAdressModalButton() {
+        await this.getInvoiceAddressModalSaveButton.click();
+    }
+
+    async clickCancelInvoiceAdressModalButton() {
+        await this.getInvoiceAddressModalCancelButton.click();
+    }
+
+    get getInvoiceAddressTitle() {
+        return this.page.locator('div[data-sentry-element="Title"]').getByText('Dane do faktury');
+    }
+
+    get getAddNewInvoiceAddressButton() {
+        return this.page.locator(`button:has-text('Dodaj nowy podmiot')`)
+    }
+
+    get getInvoiceAddressModal() {
+        return this.page.locator('div[class*="sc-f8f81ad2-1"]');
+    }
+
+    get getInvoiceAddressModalCloseIcon() {
+        return this.page.locator(selectors.DeliveryPage.common.AddressModalCloseIcon);
+    }
+
+    get getInvoiceAddressModalSaveButton() {
+        return this.page.getByRole('button', { name: 'Zapisz' });
+    }
+
+    get getInvoiceAddressModalCancelButton() {
+        return this.page.getByText('Anuluj');
+    }
+
+    get getInvoiceAddressModalConfirmationButton() {
+        return this.page.locator(`button:has-text("Potwierdź")`);
+    }
+
+    getInvoiceAddressModalDeleteAddressName(addressName: string) {
+        return this.page.locator(`div[class*="sc-f8f81ad2-1"] div[data-sentry-element="AddressName"]:has-text("${addressName}")`);
+    }
+
+    get getInvoiceAddressModalMainAddressCheckbox() {
+        return this.page.locator('#invoice_address_default label span');
+    }
+
+    get getInvoiceAddressModalAddressName() {
+        return this.page.locator('#invoice_address_name');
+    }
+
+    get getInvoiceAddressModalAddressSearchAddress() {
+        return this.page.locator('#:rku:');
+    }
+
+    get getInvoiceAddressModalCompanyName() {
+        return this.page.locator('#invoice_address_company_name');
+    }
+
+    get getInvoiceAddressModalNIP() {
+        return this.page.locator('#invoice_address_tax');
+    }
+
+    get getInvoiceAddressModalUserPostalCode() {
+        return this.page.locator('#invoice_address_postal');
+    }
+
+    get getInvoiceAddressModalUserCity() {
+        return this.page.locator('#invoice_address_city');
+    }
+
+    get getInvoiceAddressModalUserStreet() {
+        return this.page.locator('#invoice_address_street');
+    }
+
+    get getInvoiceAddressModalUserHouseNumber() {
+        return this.page.locator('#invoice_address_house_number');
+    }
+
+    get getInvoiceAddressModalUserFlatNumber() {
+        return this.page.locator('#invoice_address_flat_number');
+    }
+}
---
+++ tests/mobile/Profile/InvoiceAdresses.test.spec.ts
@@ -0,0 +1,205 @@
+import { expect } from '@playwright/test';
+import CommonPage from "../../../page/Common.page.ts";
+import InvoiceAddressesPage from '../../../page/Profile/InvoiceAddresses.page.ts';
+import * as allure from "allure-js-commons";
+import { test } from '../../../fixtures/fixtures.ts';
+import * as utility from '../../../utils/utility-methods';
+
+test.describe.configure({ mode: 'serial'})
+
+test.describe('Testy dane do faktury', async () => {
+
+  let commonPage: CommonPage;
+  let invoiceAddressesPage : InvoiceAddressesPage;
+
+  test.beforeEach(async ({ page }) => {
+
+    await page.goto('/', { waitUntil: 'commit'})
+
+    page.on('framenavigated', async () => {
+      await utility.addGlobalStyles(page);
+    });
+
+    commonPage = new CommonPage(page);
+    invoiceAddressesPage = new InvoiceAddressesPage(page);
+  })
+
+  test.afterEach(async ({ deleteInvoiceAddressViaAPI }) => {
+
+    await deleteInvoiceAddressViaAPI('Testowa nazwa podmiotu');
+    await deleteInvoiceAddressViaAPI('Fixturowy adres podmiotu')
+    await deleteInvoiceAddressViaAPI('Edytowana nazwa podmiotu')
+  })
+  
+  test('M | Strona dane do faktury otwiera się ze wszystkimi potrzebnymi polami', async ({ page }) => {
+
+    await page.goto('profil/dane-faktury', { waitUntil: 'domcontentloaded' });
+
+    await expect(invoiceAddressesPage.getInvoiceAddressTitle).toBeVisible();
+    await expect(invoiceAddressesPage.getAddNewInvoiceAddressButton).toBeVisible();
+  })
+  
+  test('M | Możliwość dodania danych do faktury', async ({ page }) => {
+
+    test.setTimeout(100000);
+    
+    await page.goto('profil/dane-faktury', { waitUntil: 'domcontentloaded' });
+
+    await invoiceAddressesPage.getInvoiceAddressTitle.waitFor({ state: 'visible', timeout: 10000 })
+
+    await invoiceAddressesPage.clickAddNewInvoiceAddressButton();
+    await expect(invoiceAddressesPage.getInvoiceAddressModal).toBeVisible();
+    await expect(invoiceAddressesPage.getInvoiceAddressModal).toContainText('Dodaj nowy podmiot');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalAddressName).toBeVisible();
+    await invoiceAddressesPage.getInvoiceAddressModalAddressName.fill('Testowa nazwa podmiotu');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalCompanyName).toBeVisible();
+    await invoiceAddressesPage.getInvoiceAddressModalCompanyName.fill('Testowa nazwa firmy');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalNIP).toBeVisible();
+    await invoiceAddressesPage.getInvoiceAddressModalNIP.fill('8140667487')
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalUserPostalCode).toBeVisible();
+    await invoiceAddressesPage.getInvoiceAddressModalUserPostalCode.fill('00-133');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalUserCity).toBeVisible();
+    await invoiceAddressesPage.getInvoiceAddressModalUserCity.fill('Warszawa');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalUserStreet).toBeVisible();
+    await invoiceAddressesPage.getInvoiceAddressModalUserStreet.fill('aleja Jana Pawła II');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalUserHouseNumber).toBeVisible();
+    await invoiceAddressesPage.getInvoiceAddressModalUserHouseNumber.fill('10');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalUserFlatNumber).toBeVisible();
+    await invoiceAddressesPage.getInvoiceAddressModalUserFlatNumber.fill('12');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalSaveButton).toBeVisible();
+    await invoiceAddressesPage.clickSaveInvoiceAdressModalButton();
+
+    await expect(commonPage.getMessage).toHaveText('Dane zostały zapisane', { timeout: 5000 })
+
+    await page.waitForSelector('text=Testowa nazwa podmiotu', { timeout: 10000, state: 'visible' });
+  })
+    
+  test('M | Możliwość edycji danych do faktury', async ({ page, addInvoiceAddressViaAPI }) => {
+
+    test.setTimeout(120000);
+
+    await addInvoiceAddressViaAPI('Fixturowy adres podmiotu');
+
+    await page.goto('profil/dane-faktury', { waitUntil: 'domcontentloaded' });
+
+    await invoiceAddressesPage.getInvoiceAddressTitle.waitFor({ state: 'visible', timeout: 10000 });
+
+    await page.waitForSelector('text=Fixturowy adres podmiotu', { timeout: 5000, state: 'visible' });
+
+    await invoiceAddressesPage.clickEditInvoiceAddressButton('Fixturowy adres podmiotu');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModal).toBeVisible();
+    await expect(invoiceAddressesPage.getInvoiceAddressModal).toContainText('Edytuj podmiot');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalAddressName).toHaveValue('Fixturowy adres podmiotu');
+    await invoiceAddressesPage.getInvoiceAddressModalAddressName.fill('Edytowana nazwa podmiotu');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalCompanyName).toHaveValue('Testowa firma');
+    await invoiceAddressesPage.getInvoiceAddressModalCompanyName.fill('Edytowana nazwa firmy');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalNIP).toHaveValue('8140667487');
+    await invoiceAddressesPage.getInvoiceAddressModalNIP.fill('1085179548')
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalUserPostalCode).toHaveValue('00-828');
+    await invoiceAddressesPage.getInvoiceAddressModalUserPostalCode.fill('00-100');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalUserCity).toHaveValue('Warszawa');
+    await invoiceAddressesPage.getInvoiceAddressModalUserCity.fill('Magdalenka');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalUserStreet).toHaveValue('aleja Jana Pawła II');
+    await invoiceAddressesPage.getInvoiceAddressModalUserStreet.fill('Edytowana ulica');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalUserHouseNumber).toHaveValue('1');
+    await invoiceAddressesPage.getInvoiceAddressModalUserHouseNumber.fill('100');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalUserFlatNumber).toHaveValue('30');
+    await invoiceAddressesPage.getInvoiceAddressModalUserFlatNumber.fill('200');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModalSaveButton).toBeVisible();
+    await invoiceAddressesPage.clickSaveInvoiceAdressModalButton();
+
+    await expect(commonPage.getMessage).toHaveText('Adres "Edytowana nazwa podmiotu" został zaktualizowany.', { timeout: 5000 });
+    await expect(commonPage.getMessage).not.toBeVisible({ timeout: 10000 });
+    
+    await page.waitForSelector('text=Edytowana nazwa podmiotu', { timeout: 5000, state: 'visible' });
+
+    await invoiceAddressesPage.clickEditInvoiceAddressButton('Edytowana nazwa podmiotu');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModal).toBeVisible();
+    await expect(invoiceAddressesPage.getInvoiceAddressModal).toContainText('Edytuj podmiot');
+    await expect(invoiceAddressesPage.getInvoiceAddressModalAddressName).toHaveValue('Edytowana nazwa podmiotu')
+    await expect(invoiceAddressesPage.getInvoiceAddressModalCompanyName).toHaveValue('Edytowana nazwa firmy');
+    await expect(invoiceAddressesPage.getInvoiceAddressModalNIP).toHaveValue('1085179548');
+    await expect(invoiceAddressesPage.getInvoiceAddressModalUserPostalCode).toHaveValue('00-100');
+    await expect(invoiceAddressesPage.getInvoiceAddressModalUserCity).toHaveValue('Magdalenka');
+    await expect(invoiceAddressesPage.getInvoiceAddressModalUserStreet).toHaveValue('Edytowana ulica');
+    await expect(invoiceAddressesPage.getInvoiceAddressModalUserHouseNumber).toHaveValue('100');
+    await expect(invoiceAddressesPage.getInvoiceAddressModalUserFlatNumber).toHaveValue('200');
+  })
+    
+  test('M | Możliwość ustawienia głównych danych do faktury', async ({ page, addInvoiceAddressViaAPI }) => {
+
+    test.setTimeout(120000);
+    
+    await addInvoiceAddressViaAPI('Fixturowy adres podmiotu');
+    await addInvoiceAddressViaAPI('Testowa nazwa podmiotu');
+
+    await page.goto('profil/dane-faktury', { waitUntil: 'domcontentloaded' });
+
+    await page.waitForSelector('text=Fixturowy adres podmiotu', { state: 'visible' });
+    await page.waitForSelector('text=Testowa nazwa podmiotu', { state: 'visible' });
+
+    await invoiceAddressesPage.clickEditInvoiceAddressButton('Testowa nazwa podmiotu');
+    await invoiceAddressesPage.getInvoiceAddressModalMainAddressCheckbox.check();
+    await invoiceAddressesPage.getInvoiceAddressModalMainAddressCheckbox.isChecked();
+    await invoiceAddressesPage.clickSaveInvoiceAdressModalButton();
+    await invoiceAddressesPage.getMainInvoiceAddressInfo('Testowa nazwa podmiotu').isVisible();
+    await expect(invoiceAddressesPage.getMainInvoiceAddressInfo('Testowa nazwa podmiotu')).toHaveText('Główny');
+    await invoiceAddressesPage.clickEditInvoiceAddressButton('Testowa nazwa podmiotu');
+    await invoiceAddressesPage.getCurrentMainInvoiceAddressModalInfo.isVisible();
+    await invoiceAddressesPage.getInvoiceAddressModalMainAddressCheckbox.isHidden();
+    await invoiceAddressesPage.clickCancelInvoiceAdressModalButton();
+
+    await invoiceAddressesPage.clickEditInvoiceAddressButton('Fixturowy adres podmiotu');
+    await invoiceAddressesPage.getInvoiceAddressModalMainAddressCheckbox.check();
+    await invoiceAddressesPage.getInvoiceAddressModalMainAddressCheckbox.isChecked();
+    await invoiceAddressesPage.clickSaveInvoiceAdressModalButton();
+    await invoiceAddressesPage.getMainInvoiceAddressInfo('Fixturowy adres podmiotu').isVisible();
+    await expect(invoiceAddressesPage.getMainInvoiceAddressInfo('Fixturowy adres podmiotu')).toHaveText('Główny');
+    await invoiceAddressesPage.getMainInvoiceAddressInfo('Testowa nazwa podmiotu').isHidden();
+    await expect(invoiceAddressesPage.getMainInvoiceAddressInfo('Testowa nazwa podmiotu')).not.toBeAttached({ timeout: 5000 });
+  })
+        
+  test('M | Możliwość usunięcia danych do faktury', async ({ page, addInvoiceAddressViaAPI }) => {
+      
+    test.setTimeout(120000);
+
+    await addInvoiceAddressViaAPI('Edytowana nazwa podmiotu');
+    
+    await page.goto('profil/dane-faktury', { waitUntil: 'domcontentloaded' });
+
+    await invoiceAddressesPage.getInvoiceAddressTitle.waitFor({ state: 'visible', timeout: 10000 });
+
+    await invoiceAddressesPage.clickDeleteInvoiceAddressButton('Edytowana nazwa podmiotu');
+
+    await expect(invoiceAddressesPage.getInvoiceAddressModal).toBeVisible({ timeout: 3000 });
+    await expect(invoiceAddressesPage.getInvoiceAddressModal).toContainText('Potwierdź usunięcie adresu');
+    await expect(invoiceAddressesPage.getInvoiceAddressModalDeleteAddressName('Edytowana nazwa podmiotu')).toContainText('Edytowana nazwa podmiotu');
+    await expect(invoiceAddressesPage.getInvoiceAddressModalCancelButton).toBeVisible();
+    await expect(invoiceAddressesPage.getInvoiceAddressModalConfirmationButton).toBeVisible();
+    await invoiceAddressesPage.getInvoiceAddressModalConfirmationButton.click();
+    await expect(commonPage.getMessage).toHaveText('Adres "Edytowana nazwa podmiotu" został usunięty.', { timeout: 5000 });
+    await expect(commonPage.getMessage).not.toBeVisible({ timeout: 10000 });
+
+    await page.waitForSelector('text=Edytowana nazwa podmiotu', { strict: true , state: 'hidden' });
+  })
+})
